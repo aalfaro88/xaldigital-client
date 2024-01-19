09a8090f0bd4887a3485a95cf967ea5c9b17fb2c
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2024-01-19 17:33:57 -0600

    Ready For Deployment

--- src/services/SERVER_URL.js
+++ src/services/SERVER_URL.js
@@ -5,7 +5,7 @@ let SERVER_URL;
 if (window.location.host.includes("localhost:5173")) {
   SERVER_URL = "http://localhost:4000";
 } else {
-  SERVER_URL = "TBD";
+  SERVER_URL = "https://xaldigitalserver.adaptable.app";
 }
 
 export { SERVER_URL };
